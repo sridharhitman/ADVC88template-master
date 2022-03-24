de0251d2f6dd7ec6089f3602f74860530e9426b4
sridharhitman <83956232+sridharhitman@users.noreply.github.com>
2022-03-24 14:12:35 +0530

    bug fixes

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var canvas = new fabric.Canvas('myCanvas')
+var canvas = new fabric.Canvas('myCanvas');
 // Create canvas variable
 ball_x = 0;
 ball_y = 0;
@@ -12,31 +12,31 @@ block_image_width = 5;
 block_image_height = 5;
 
 function load_img(){
-	fabric.Image.fromURl('golf-h1.png', function(Img){
+	fabric.Image.fromURL('golf-h1.png', function(Img){
 		hole_obj = Img;
 		hole_obj.scaleToWidth(50);
 		hole_obj.scaleToHeight(50);
-		hole_onj.set({
+		hole_obj.set({
 			top: hole_y,
 			left:hole_x,
 		});
-		canvas.add(hole_obj)
-	})
+		canvas.add(hole_obj);
+	});
 	new_image();
 }
 
 function new_image()
 {
-	fabric.Image.fromURl('ball.png', function(Img){
+	fabric.Image.fromURL('ball.png', function(Img){
 		ball_obj = Img;
 		ball_obj.scaleToWidth(50);
 		ball_obj.scaleToHeight(50);
-		ball_onj.set({
+		ball_obj.set({
 			top: ball_y,
 			left:ball_x,
 		});
-		canvas.add(ball_obj)
-	})
+		canvas.add(ball_obj);
+	});
 }
 
 window.addEventListener("keydown", my_keydown);
@@ -46,9 +46,9 @@ function my_keydown(e)
 	keyPressed = e.keyCode;
 	console.log(keyPressed);
 	if(ball_x == hole_x && ball_y == hole_y){
-		canvas.remove(ball_obj)
+		canvas.remove(ball_obj);
 		document.getElementById("hd3").innerHTML = "You have hit the goal!!";
-		document.getElementById("myCanvas").style.borderColor = "red"
+		document.getElementById("myCanvas").style.borderColor = "red";
 	}
 	
 	else{
@@ -77,9 +77,10 @@ function my_keydown(e)
 	function up()
 	{
 		if(ball_y <= 450){
-			ball_y = ball_y + block_image_height;
-			console.log("Block Image height =" , block_image_height)
-			console.log("X =" , ball_x , "Y =" , ball_y)
+			ball_y = ball_y - block_image_height;
+			console.log("Block Image height =" , block_image_height);
+			console.log("X =" , ball_x , "Y =" , ball_y);
+			canvas.remove(ball_obj);
 			new_image();
 		}
 		
@@ -89,8 +90,9 @@ function my_keydown(e)
 	{
 		if(ball_y <= 450){
 			ball_y = ball_y + block_image_height;
-			console.log("Block Image height =" , block_image_height)
-			console.log("X =" , ball_x , "Y =" , ball_y)
+			console.log("Block Image height =" , block_image_height);
+			console.log("X =" , ball_x , "Y =" , ball_y);
+			canvas.remove(ball_obj);
 			new_image();
 		}
 	}
@@ -100,8 +102,9 @@ function my_keydown(e)
 		if(ball_x >5)
 		{
 			ball_x = ball_x - block_image_width;
-			console.log("Block Image Width =" , block_image_width)
-			console.log("X =" , ball_x , "Y =" , ball_y)
+			console.log("Block Image Width =" , block_image_width);
+			console.log("X =" , ball_x , "Y =" , ball_y);
+			canvas.remove(ball_obj);
 			new_image();
 		}
 	}
@@ -111,8 +114,9 @@ function my_keydown(e)
 		if(ball_x <=1050)
 		{
 			ball_x = ball_x + block_image_width;
-			console.log("Block Image Width =" , block_image_width)
-			console.log("X =" , ball_x , "Y =" , ball_y)
+			console.log("Block Image Width =" , block_image_width);
+			console.log("X =" , ball_x , "Y =" , ball_y);
+			canvas.remove(ball_obj);
 			new_image();
 			
 		}
